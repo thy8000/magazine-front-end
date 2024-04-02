113348389096f80c9e607c7754360a46fc42dd67
Thunay Moreira de Soares <thunaymoreiradesoares@gmail.com.br>
2024-04-01 23:09:53 -0300

    🚧 Front page - Content

--- src/wp-templates/FrontPage/components/Content/Content.tsx
+++ src/wp-templates/FrontPage/components/Content/Content.tsx
@@ -1,7 +1,33 @@
 export default function Content({themeOptionsHome}: any) {
+    const data = getHomePostsList(themeOptionsHome);
+
+    if(data.length == 0) {
+        return;
+    }
+
+    console.log(data);
+
     return (
         <div>
             Content
         </div>
     )
+}
+
+function getHomePostsList(data: any) {
+    const dataObject = data?.data ?? [];
+
+    if(dataObject.length == 0) {
+        return [];
+    }
+
+    const dataJSON = JSON.parse(dataObject);
+
+    if(dataJSON.length == 0) {
+        return [];
+    }
+
+    const postsLists = dataJSON?.homePostsList ?? [];
+
+    return postsLists;
 }
